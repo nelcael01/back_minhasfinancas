4a8bf5cfa05eed839e0871f130611170819bc2cc
nelcael07 <47829164+nelcael07@users.noreply.github.com>
2023-07-20 16:50:08 -0400

    Implemnteção do lombok no projeto

--- src/main/java/com/nelcael/minhasfinancas/model/entity/Lancamento.java
+++ src/main/java/com/nelcael/minhasfinancas/model/entity/Lancamento.java
@@ -18,9 +18,17 @@ import jakarta.persistence.Convert;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.JoinColumn;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Table(name = "lancamento", schema="financas")
+@Data
 public class Lancamento {
 	
 	@Id
@@ -55,106 +63,5 @@ public class Lancamento {
 	@Column(name = "status")
 	@Enumerated(value = EnumType.STRING)
 	private StatusLancamento status;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public Integer getMes() {
-		return mes;
-	}
-
-	public void setMes(Integer mes) {
-		this.mes = mes;
-	}
-
-	public Integer getAno() {
-		return ano;
-	}
-
-	public void setAno(Integer ano) {
-		this.ano = ano;
-	}
-
-	public String getDescricao() {
-		return descricao;
-	}
-
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
-
-	public Usuario getUsuario() {
-		return usuario;
-	}
-
-	public void setUsuario(Usuario usuario) {
-		this.usuario = usuario;
-	}
-
-	public BigDecimal getValor() {
-		return valor;
-	}
-
-	public void setValor(BigDecimal valor) {
-		this.valor = valor;
-	}
-
-	public LocalDate getDataCadastro() {
-		return dataCadastro;
-	}
-
-	public void setDataCadastro(LocalDate dataCadastro) {
-		this.dataCadastro = dataCadastro;
-	}
-
-	public TipoLancamento getTipo() {
-		return tipo;
-	}
-
-	public void setTipo(TipoLancamento tipo) {
-		this.tipo = tipo;
-	}
-
-	public StatusLancamento getStatus() {
-		return status;
-	}
-
-	public void setStatus(StatusLancamento status) {
-		this.status = status;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(ano, dataCadastro, descricao, id, mes, status, tipo, usuario, valor);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Lancamento other = (Lancamento) obj;
-		return Objects.equals(ano, other.ano) && Objects.equals(dataCadastro, other.dataCadastro)
-				&& Objects.equals(descricao, other.descricao) && Objects.equals(id, other.id)
-				&& Objects.equals(mes, other.mes) && status == other.status && tipo == other.tipo
-				&& Objects.equals(usuario, other.usuario) && Objects.equals(valor, other.valor);
-	}
-
-	@Override
-	public String toString() {
-		return "Lancamento [id=" + id + ", mes=" + mes + ", ano=" + ano + ", descricao=" + descricao + ", usuario="
-				+ usuario + ", valor=" + valor + ", dataCadastro=" + dataCadastro + ", tipo=" + tipo + ", status="
-				+ status + "]";
-	}
-	
-	
 	
 }
--- src/main/java/com/nelcael/minhasfinancas/model/entity/Usuario.java
+++ src/main/java/com/nelcael/minhasfinancas/model/entity/Usuario.java
@@ -1,19 +1,26 @@
 package com.nelcael.minhasfinancas.model.entity;
 
-import java.util.Objects;
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
-import jakarta.annotation.Generated;
 import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 
 @Entity
 @Table( name = "usuario", schema = "financas ")
+@Data
 public class Usuario {
 
 	@Id
@@ -28,63 +35,7 @@ public class Usuario {
 	private String email;
 	
 	@Column(name = "senha")
-	private String senha;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getSenha() {
-		return senha;
-	}
-
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(email, id, nome, senha);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Usuario other = (Usuario) obj;
-		return Objects.equals(email, other.email) && Objects.equals(id, other.id) && Objects.equals(nome, other.nome)
-				&& Objects.equals(senha, other.senha);
-	} 
-	
-	@Override
-	public String toString() {
-		return "Usuario [id=" + id + ", nome=" + nome + ", email=" + email + ", senha=" + senha + "]";
-	}
-	
+	private String senha;	
 	
 }
 	
