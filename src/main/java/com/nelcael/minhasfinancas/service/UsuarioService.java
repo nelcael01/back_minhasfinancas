ac296da6ebdb21fb78a9033cd047ca064ef8d7e1
nelcael07 <47829164+nelcael07@users.noreply.github.com>
2023-07-24 15:38:48 -0400

    Adição do banco de teste em memória h2

--- src/main/java/com/nelcael/minhasfinancas/service/UsuarioService.java
+++ src/main/java/com/nelcael/minhasfinancas/service/UsuarioService.java
@@ -3,7 +3,6 @@ package com.nelcael.minhasfinancas.service;
 import com.nelcael.minhasfinancas.model.entity.Usuario;
 
 public interface UsuarioService {
-
     Usuario autenticar(String email, String senha);
 
     Usuario salvarUsuario(Usuario usuario);
--- src/test/java/com/nelcael/minhasfinancas/model/repository/UsuarioRepositoryTest.java
+++ src/test/java/com/nelcael/minhasfinancas/model/repository/UsuarioRepositoryTest.java
@@ -6,10 +6,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
+@ActiveProfiles("test")
 public class UsuarioRepositoryTest {
     @Autowired
     UsuarioRespository respository;
@@ -19,8 +21,10 @@ public class UsuarioRepositoryTest {
         //Cenário
         Usuario usuario = Usuario.builder().nome("usuario").email("usuario@email.com").build();
         respository.save(usuario);
+
         //Ação
         boolean resultado = respository.existsByEmail(usuario.getEmail());
+
         //Verificação
         Assertions.assertThat(resultado).isTrue();
     }
