431964a0a33852a0080043d0fe6c6154309efd9a
nelcael07 <47829164+nelcael07@users.noreply.github.com>
2023-07-24 20:39:00 -0400

    Implementação dos testes unitários de salvarUsuario

--- src/test/java/com/nelcael/minhasfinancas/service/UsuarioServiceTest.java
+++ src/test/java/com/nelcael/minhasfinancas/service/UsuarioServiceTest.java
@@ -9,8 +9,10 @@ import org.assertj.core.api.Assertions;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -20,14 +22,11 @@ import java.util.Optional;
 @ActiveProfiles("test")
 public class UsuarioServiceTest {
 
-    UsuarioService service;
+    @SpyBean
+    UsuarioServiceImplementacao service;
     @MockBean
     UsuarioRespository repository;
 
-    @Before
-    public void setUp() {
-        service = new UsuarioServiceImplementacao(repository);
-    }
 
     @Test(expected = Test.None.class)
     public void deveValidarEmail() {
@@ -53,21 +52,47 @@ public class UsuarioServiceTest {
         Assertions.assertThat(result).isNotNull();
     }
 
-    @Test(expected = ErroAutenticacao.class)
+    @Test
     public void naoDeveAutenticarUsuarioPorJaTerEmailCadastrado() {
         Mockito.when(repository.findByEmail(Mockito.anyString())).thenReturn(Optional.empty());
 
-        service.autenticar("email@gmail.com", "senha");
+        Throwable exception = Assertions.catchThrowable(() -> service.autenticar("email@gmail.com", "senha"));
+        Assertions.assertThat(exception).isInstanceOf(ErroAutenticacao.class).hasMessage("Esse e-mail não foi cadastrado");
     }
 
-    @Test(expected = ErroAutenticacao.class)
-    public void naoDeveAutenticarUsuarioPorTerSenhaErrada(){
+    @Test
+    public void naoDeveAutenticarUsuarioPorTerSenhaErrada() {
         String senha = "senha";
         Usuario usuario = Usuario.builder().email("email@gmail.com").senha(senha).build();
         Mockito.when(repository.findByEmail(Mockito.anyString())).thenReturn(Optional.of(usuario));
 
-        service.autenticar("email@gmail.com","123");
+        Throwable exception = Assertions.catchThrowable(() -> service.autenticar("email@gmail.com", "123"));
+        Assertions.assertThat(exception).isInstanceOf(ErroAutenticacao.class).hasMessage("A senha é inválida");
     }
 
+    @Test(expected = Test.None.class)
+    public void deveSalvarUmUsuario() {
+        Mockito.doNothing().when(service).validarEmail(Mockito.anyString());
+        Usuario usuario = Usuario.builder().nome("usuario").email("email@gmail.com").senha("123").id(1L).build();
+        Mockito.when(repository.save(Mockito.any(Usuario.class))).thenReturn(usuario);
+
+        Usuario result = service.salvarUsuario(new Usuario());
+        Assertions.assertThat(result).isNotNull();
+        Assertions.assertThat(result.getId()).isEqualTo(usuario.getId());
+        Assertions.assertThat(result.getNome()).isEqualTo(usuario.getNome());
+        Assertions.assertThat(result.getEmail()).isEqualTo(usuario.getEmail());
+        Assertions.assertThat(result.getSenha()).isEqualTo(usuario.getSenha());
+    }
+
+    @Test(expected = RegraNegocioException.class)
+    public void naoDeveSalvarUmUsuarioComEmailCadastrado() {
+        Usuario usuario = Usuario.builder().email("email@gmail.com").build();
+        Mockito.doThrow(RegraNegocioException.class).when(service).validarEmail(usuario.getEmail());
+
+        service.salvarUsuario(usuario);
+
+        Mockito.verify(repository, Mockito.never()).save(usuario);
+
+    }
 
 }
