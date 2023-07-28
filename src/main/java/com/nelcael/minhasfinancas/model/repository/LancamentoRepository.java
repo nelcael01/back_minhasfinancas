5570e3a8d17fcadab0b44851ed0eb7638487c63a
nelcael07 <47829164+nelcael07@users.noreply.github.com>
2023-07-28 16:24:07 -0400

    Implementação do teste de integração atualizar e buscarPorId de Lancamento - Finalização de testes de LancamentoRepository

--- src/main/java/com/nelcael/minhasfinancas/model/repository/LancamentoRepository.java
+++ src/main/java/com/nelcael/minhasfinancas/model/repository/LancamentoRepository.java
@@ -10,12 +10,15 @@ import org.springframework.data.repository.query.Param;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 public interface LancamentoRepository extends JpaRepository<Lancamento, Long>{
 
     Lancamento save(Lancamento lancamento);
     void delete(Lancamento lancamento);
+    Optional<Lancamento> findById(Long id);
     List<Lancamento> findAll(Example example);
+
     @Query(value = "select sum(l.valor) from Lancamento l join l.usuario u" +
             " where u.id = :idUsuario and l.tipo =:tipo group by u")
     BigDecimal obterSaldoPorTipoLancamentoEUsuario(@Param("idUsuario") Long idUsuario, @Param("tipo") TipoLancamento tipo);
--- src/test/java/com/nelcael/minhasfinancas/model/repository/LancamentoRepositoryTest.java
+++ src/test/java/com/nelcael/minhasfinancas/model/repository/LancamentoRepositoryTest.java
@@ -3,7 +3,8 @@ package com.nelcael.minhasfinancas.model.repository;
 import com.nelcael.minhasfinancas.model.entity.Lancamento;
 import com.nelcael.minhasfinancas.model.enuns.StatusLancamento;
 import com.nelcael.minhasfinancas.model.enuns.TipoLancamento;
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import javax.persistence.EntityManager;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Optional;
 
 @RunWith(SpringRunner.class)
 @ActiveProfiles("test")
@@ -29,34 +31,62 @@ public class LancamentoRepositoryTest {
     TestEntityManager entityManager;
 
     @Test
-    public void deveSalvarUmLancamento(){
+    public void deveSalvarUmLancamento() {
         Lancamento lancamento = criarLancamento();
         lancamento = repository.save(lancamento);
-        Assertions.assertThat(lancamento.getId()).isNotNull();
+        assertThat(lancamento.getId()).isNotNull();
     }
 
     @Test
-    public void deveDeletarUmLancamento(){
-        Lancamento lancamento = criarLancamento();
-        Lancamento lancamentoSalvo = entityManager.persist(lancamento);
+    public void deveDeletarUmLancamento() {
+        Lancamento lancamentoSalvo = criarEPersistirUmLancamento();
         lancamentoSalvo = entityManager.find(Lancamento.class, lancamentoSalvo.getId());
         repository.delete(lancamentoSalvo);
 
         Lancamento lancamentoInexistente = entityManager.find(Lancamento.class, lancamentoSalvo.getId());
-        Assertions.assertThat(lancamentoInexistente).isNull();
+        assertThat(lancamentoInexistente).isNull();
     }
 
-     private Lancamento criarLancamento(){
-         return Lancamento.builder()
-                 .ano(2019)
-                 .mes(1)
-                 .descricao("Lancamento qualquer")
-                 .valor(BigDecimal.valueOf(10))
-                 .tipo(TipoLancamento.RECEITA)
-                 .status(StatusLancamento.PENDENTE)
-                 .dataCadastro(LocalDate.now())
-                 .build();
-     }
+    @Test
+    void deveAtualizarUmLancamento() {
+        Lancamento lancamento = criarEPersistirUmLancamento();
+
+        lancamento.setAno(2023);
+        lancamento.setMes(5);
+        lancamento.setDescricao("Teste Atualizado");
+
+        repository.save(lancamento);
+
+        Lancamento lancamentoAtualizado = entityManager.find(Lancamento.class, lancamento.getId());
+        assertThat(lancamentoAtualizado.getAno()).isEqualTo(2023);
+        assertThat(lancamentoAtualizado.getMes()).isEqualTo(5);
+        assertThat(lancamentoAtualizado.getDescricao()).isEqualTo("Teste Atualizado");
+    }
+    @Test
+    void deveEncontrarUmLancamentoPorId(){
+        Lancamento lancamentoSalvo = criarEPersistirUmLancamento();
+        Optional<Lancamento> lancamentoBuscado = repository.findById(lancamentoSalvo.getId());
+        assertThat(lancamentoBuscado.isPresent()).isTrue();
+
+    }
+
+    private Lancamento criarEPersistirUmLancamento() {
+        Lancamento lancamento = criarLancamento();
+        Lancamento lancamentoSalvo = entityManager.persist(lancamento);
+        return lancamentoSalvo;
+    }
+
+    private Lancamento criarLancamento() {
+        return Lancamento.builder()
+                .ano(2019)
+                .mes(1)
+                .descricao("Lancamento qualquer")
+                .valor(BigDecimal.valueOf(10))
+                .tipo(TipoLancamento.RECEITA)
+                .status(StatusLancamento.PENDENTE)
+                .dataCadastro(LocalDate.now())
+                .build();
+    }
 
 
 }
